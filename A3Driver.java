b88519b785140546494d601dd6bb5875dbe44bf4
sbordov <stefan.bordovsky@gmail.com>
2016-02-23 22:18:55 -0600

    Fixed a bug where Electronics were being instantiated as Clothing in insert().

--- A3Driver.java
+++ A3Driver.java
@@ -26,7 +26,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.Comparator;
 
 
 
@@ -36,17 +35,6 @@ public class A3Driver
 	  public static void main(String[] args) 
 	  {
  
-		/*
-		 * I (Stefan) added this stretch of code. The rest came in the default package.
-		 * The current design plan is to use processLinesInFile to comb through the input
-		 * data and remove lines with improper input arguments. The String array named
-		 * transactions should then be run through a separate method that actually performs
-		 * necessary commands. String[] transaction will be a single transaction line.
-		 * Before executing each transaction line, a method should check to see
-		 * if it can actually perform that operation (e.g. a transaction line with the
-		 * operand "update" should only go through transaction if there is an object of
-		 * the listed name already in the ArrayList). 
-		 */
 		//Open file; file name specified in args (command line)
 		if (args.length != 1) 
 		{
@@ -54,26 +42,10 @@ public class A3Driver
 			System.exit(-1);
 		}
 		String[] transactions = processLinesInFile (args[0]);
-		  
-		//Parse input, take appropriate actions.
-			
-		//Stub for arraylist.
 		ArrayList<Item> shoppingCart = new ArrayList<Item>(); 
 		processTransactions(transactions, shoppingCart);
 		
-		// General code example for how to iterate an array list. You will have to modify this heavily, to suit your needs.
-		/* Useless starter code
-		Iterator<Item> i = shoppingCart.iterator();
-		while (i.hasNext()) 
-		{
-			Item temp = i.next();
-			temp.calculatePrice(); 
-			temp.printItemAttributes();
-			//This (above) works because of polymorphism: a determination is made at runtime, 
-			//based on the inherited class type, as to which method is to be invoked. Eg: If it is an instance
-			// of Grocery, it will invoke the calculatePrice () method defined in Grocery.
-		}		
-		*/
+		return;
 	  }
 	  
 	  // I (Stefan) added everything below this line.((Edit)-Dan Coded the meat of the function reviewTransaction)
@@ -135,7 +107,6 @@ public class A3Driver
 		*         								                                      *
 		* Returns: boolean indicating validity of input input transaction.            *
 		******************************************************************************/
-		//Dan's Code is here 
 		public static boolean reviewTransaction (String transaction){
 			 ValidInput parsedArgs = new ValidInput();
 			 parsedArgs.inputArgs = transaction.split("\\s+"); // Parses transaction.
@@ -226,7 +197,7 @@ public class A3Driver
 				case "electronics":
 					op1 = transaction[6];
 					op2 = transaction[7];
-					item = new Grocery(name, price, quantity, weight, op2);
+					item = new Electronics(name, price, quantity, weight, op1, op2);
 					break;
 				default: System.out.println("Error in insert.");
 			}
@@ -332,64 +303,5 @@ public class A3Driver
 		    return low;
 		}
 	}
-			  
-			 /*
-			 //firstly, our inputs should have a minimum of 6 inputs, and could have up to 8 
-			 if(parsedArgs.inputArgs.length < 6||parsedArgs.inputArgs.length > 8)	
-			 {
-				 System.out.println("Invalid Input Length");
-				 return false;
-			 }		
-			 //Added a try/catch just to ensure there are no reads that should not be implemented
-			 try
-			 {
-				 
-				 if(!parsedArgs.isValidOp(parsedArgs.inputArgs[0]))
-				 {
-					 System.out.println("Sorry, that is not a valid operation. Try again");
-					 return false;
-				 
-				 }
-				 
-			 // Check for valid category.
-				 else if(!parsedArgs.isValidCat(parsedArgs.inputArgs[1]))
-				 { 
-					 System.out.println("Sorry, that is not a valid category. Try again");
-					 return false;
-				 }
-				// Assume all names are valid.
-				// Check for valid price. Can price = 0?----------------------Assumption is yes for now. 
-				 else if(!parsedArgs.isValidPrice(parsedArgs.inputArgs[3])){
-					 System.out.println("Sorry, that is not a valid price input");
-					 return false;
-				 }
-				 // Check for valid Quantity. Can quantity = 0? =========Again assumption will be yes for now.
-				 else if(!parsedArgs.isValidInt(parsedArgs.inputArgs[4]))
-				 {
-					 System.out.println("Sorry, that is not a valid quantity, isValidInt Error. Try again");
-					 return false;
-				 } 
-			
-				 //Checking for valid weight
-				 else if(!parsedArgs.isValidInt(parsedArgs.inputArgs[5])){
-					 System.out.println("Sorry, that is not a valid weight. Try again");
-					 return false;
-				 }
-				 //Checking for Op1 Validity, inputs to the checker function in ValidInput are
-				  * (input,category), both of type String where input refers to the op1 field (inputArgs[6]) 
-				  * and category refers to inputArgs[1]
-				  //
-				 else if(!parsedArgs.isValidOp1(parsedArgs.inputArgs[6], parsedArgs.inputArgs[1])){
-					 
-
-				 }else if(!parsedArgs.isValidOp1(parsedArgs.inputArgs[7], parsedArgs.inputArgs[1]))
-			 }
-			 catch (Exception e)
-			 {
-				 System.out.println("You have an invalidity in input somewhere causing an index to be read that is out of bounds");
-				 return false;
-				 
-			 }
-			 */
 		
 
--- ValidInput.java
+++ ValidInput.java
@@ -267,7 +267,7 @@ public class ValidInput{
 		}
 	}
 	
-	public  String[] states = {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL",
+	private  String[] states = {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL",
 			"GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", 
 			"MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR",
 			"PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
